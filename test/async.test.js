576a5996a7d4ca978b0169ee7b45ad610a3e2452
蒋勇 <jiangy@highzap.com>
2020-02-13 16:20:41 +0800

    add test use case

--- test/async.test.js
+++ test/async.test.js
@@ -1,10 +1,11 @@
-require("should")
 const fs = require("fs");
+require("should");
 
 describe("Async Test", function () {
     it("The file test.txt content should be Jiangy", function (done) {
-        fs.readFile("./test.txt", "utf-8", function (err, data) {
-            data.should.eql("Kvkens");
+        fs.readFile("test.txt", "utf-8", function (err, data) {
+            should.equal(err, null);
+            data.should.eql("Jiangy");
             done();
         })
     })
